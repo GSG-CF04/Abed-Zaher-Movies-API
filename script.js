22025ca4538a16631288979df1989dd97e13409e
zaher-aa <zaherabuamro@gmail.com>
2021-11-18 13:06:21 +0200

    Added Scroll To Top Button Relates #09

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-"use strict";
+// Scroll To Top
+const btn = document.querySelector("#scroll-to-top");
+
+btn.onclick = () => {
+  this.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+window.onscroll = () => {
+  if (window.scrollY >= 50) {
+    btn.classList.add("not-active");
+  } else btn.classList.remove("not-active");
+};
+
+("use strict");
 
 const baseURL = "https://yts.mx/api/v2/list_movies.json";
 
